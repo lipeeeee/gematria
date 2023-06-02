eeb607d9f7c7f1153a9171d928c27c1fc3cfa180
lipeeeee <filipe.pereirafga@gmail.com>
2023-06-02 15:15:39 +0100

    custom hashing script for gematria

---
+++ hash.py
@@ -0,0 +1,20 @@
+"""
+    Hashing script for the deepweb hash using the Gematria library(./lib/)
+
+    - Takes in a file input with '\n' separated values
+    to be hashed by the various hash funtions and compared to the deepweb hash
+"""
+
+import sys
+from __future__ import annotations
+
+def main() -> int:
+    """Hashing entry point"""
+    return 0
+
+def custom_parse() -> Tuple:
+    """."""
+    ...
+
+if __name__ == "__main__":
+    sys.exit(main())
