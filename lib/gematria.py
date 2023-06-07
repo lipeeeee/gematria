ce7b10b086baef74697bd80ef49378484e1068ff
lipeeeee <filipe.pereirafga@gmail.com>
2023-06-07 16:45:42 +0100

    Gematria.py documentation

--- lib/gematria.py
+++ lib/gematria.py
@@ -1,8 +1,14 @@
+"""
+    All of GEMATRIA's alphabet logic n latin
+"""
+
 import math
 
-
 class Gematria:
-    def __init__(self):
+     """Gematria Alphabet"""
+
+     def __init__(self) -> None:
+        """https://cdn.discordapp.com/attachments/787683114914545684/787683150418542632/GematrriaPrimus.jpg"""       
         self.gematriaprimus = (
             (" ", " ", 0),
             (u"ᚠ", "f", 2),
@@ -40,6 +46,7 @@ class Gematria:
             (u"ᛡ", "I", 107),  # io
             (u"ᛠ", "X", 109),  # ea
         )
+
         self.latsimple = (
             ("T", "th"),
             ("E", "eo"),
@@ -52,51 +59,60 @@ class Gematria:
             ("X", "ea"),
         )
 
-    # algorithm taken from here: https://pastebin.com/6v1XC1kV
     def gem_map(self, x, src, dest):
-        m = {p[src]: p[dest] for p in self.gematriaprimus}
+        """."""
+		m = {p[src]: p[dest] for p in self.gematriaprimus}
         return [m[c] if c in m else c for c in x]
 
     def lat_to_sim(self, x):
-        x = x.lower().replace("qu", "kw").replace("q", "k")
+       	"""Get simplified version of latin letter(supports arrays)"""
+		x = x.lower().replace("qu", "kw").replace("q", "k")
         for sim in self.latsimple:
             x = x.replace(sim[1], sim[0])
         return x
 
     def sim_to_lat(self, x):
+		"""Get latin version of simplified letter(supports arrays)"""
         for sim in self.latsimple:
             x = x.replace(sim[0], sim[1])
         return x
 
     def run_to_lat(self, x):
+		"""Get runic equivalent of latin letter"""
         return self.sim_to_lat("".join(self.gem_map(x, 0, 1)))
 
     def run_to_num(self, x):
+		"""Get gematria value of rune"""
         return self.gem_map(x, 0, 2)
 
     def lat_to_run(self, x):
+		"""Get latin equivalent of rune"""
         return "".join(self.gem_map(self.lat_to_sim(x), 1, 0))
 
     def lat_to_num(self, x):
+		"""Get gematria value of latin letter"""
         # strip non alpha chars when converting to num
         x = "".join([c for c in x if c.isalpha() or c == " "])
         return self.gem_map(self.lat_to_sim(x), 1, 2)
 
     def num_to_run(self, x):
+		"""Given a value, get it's rune in the gematria alphabet"""
         return self.gem_map(x, 2, 0)
 
     def num_to_lat(self, x):
+		"""Given a value, get it's latin letter inj the gematria alphabet"""
         return self.sim_to_lat("".join(self.gem_map(x, 2, 1)))
 
-
 class Cipher:
+	"""Cipher repr class"""
+
     def __repr__(self):
         return "<Cipher %s>" % (self.text)
 
     def __str__(self):
         return self.text
 
-    def __init__(self, text, alpha):
+    def __init__(self, text, alpha) -> None:
         self.text = text
         self.alpha = alpha
         self.gm = Gematria()
@@ -113,38 +129,49 @@ class Cipher:
         )
 
     def to_runes(self):
+		"""Latin to runes"""
         return Runes(self.gm.lat_to_run(self.text))
 
     def to_latin(self):
+		"""Runes to latin"""
         return Latin(self.gm.run_to_lat(self.text))
 
     def to_numbers(self):
+		"""X to Numeric value"""
         return self.gm.run_to_num(self.gm.lat_to_run(self.text))
 
     def sub(self, plain, cipher):
+		"""Substitute plain with cipher"""
         self.text = self.text.upper()
         return Cipher(self.text.translate(str.maketrans(plain, cipher)), self.alpha)
 
     def shift(self, n):
+		"""Shift Cipher"""
         return self.sub(self.alpha, self.alpha[n:] + self.alpha[:n])
 
     def atbash(self):
+		"""Atbash value of X"""
         return self.sub(self.alpha, self.alpha[::-1])
 
     def gematria_sum(self):
+		"""Gematria sum of self.text"""
         return sum([n for n in self.to_numbers() if type(n) is int])
 
     def gematria_sum_words(self):
+		"""Gematria sum of words in self.text"""
         return [Runes(w).gematria_sum() for w in self.text.split()]
 
     def gematria_sum_lines(self):
+		"""Sum of Gematria in self.text lines"""
         return [Runes(w).gematria_sum() for w in self.text.splitlines()]
 
     def to_index(self):
+		"""Get index of X in Gematria alphabet"""
         return [self.alpha.index(i.upper()) for i in self.text.upper()]
 
     def running_shift(self, key, interrupts="", skip_indices=[], decrypt=True):
-        if not key:
+        """."""
+		if not key:
             return self.text
 
         # handles modulo
@@ -180,11 +207,13 @@ class Cipher:
         return Cipher(o, self.alpha)
 
     def vigenere(self, key, interrupts=[], decrypt=True):
-        key = [self.alpha.index(k) for k in key.upper() if k in self.alpha]
+        """Vigenere cipher"""
+		key = [self.alpha.index(k) for k in key.upper() if k in self.alpha]
         return self.running_shift(key, interrupts=interrupts, decrypt=decrypt)
 
     def totient_stream(self, interrupts="", skip_indices=[], decrypt=True):
-        return self.running_shift(
+        """Totient Encoding"""
+		return self.running_shift(
             (p - 1 for p in self.primes()),
             interrupts=interrupts,
             skip_indices=skip_indices,
@@ -192,15 +221,21 @@ class Cipher:
         )
 
 class Runes(Cipher):
-    def __init__(self, text):
+    """Runes alphabet"""
+
+	def __init__(self, text) -> None:
         super().__init__(text, "ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ")
 
 class Latin(Cipher):
-    def __init__(self, text):
+	"""Latin Alphabet"""
+    
+	def __init__(self, text) -> None:
         super().__init__(text.upper(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 class Hex(Cipher):
-    def __init__(self, text):
+	"""Hex Cipher"""
+
+    def __init__(self, text) -> None:
         super().__init__(text.upper(), "0123456789ABCDEF")
 
 if __name__ == "__main__":
